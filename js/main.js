79608d282fbb8d4a35dd7b20d7d9464dbd4ee3a5
Luke Woodward <luke.wdwrd@gmail.com>
2017-03-10 18:09:45 -0600

    created sqlite file

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+//calling sqlite3 ; targeting Database obj using ES6 deconstr
+//.verbose() helps debug errors
+const { Database } = require('sqlite3').verbose();
+
+// returns a new database obj and automatically opens the database : accepts a callback confirming connection
+const bangDB = new Database('bangazon.sqlite', () => console.log('Connection!'));
